519eef9e168bc6f2594526ddde00eb7d37dccb15
Sunho <sunho@Sunhos-MacBook-Pro.local>
2024-08-09 19:39:36 +0900

    feat: make gitignore and project

---
+++ Boleto/Boleto/BoletoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoletoApp.swift
+//  Boleto
+//
+//  Created by Sunho on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BoletoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
