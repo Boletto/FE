31edcc1438571ab834f7838bcb203f511edee62e
Sunday <sunhofficial@github.com>
2024-08-11 15:40:32 +0900

    feat: make MainTabbar of 3 enum and custom top tabbar at mainTravelView

--- Boleto/Boleto/ContentView.swift
+++ Boleto/Boleto/ContentView.swift
@@ -6,19 +6,37 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
+    @State var model: TabModel = TabModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: self.$model.selectedTab) {
+            Group {
+                Text("Tab Content 1").tabItem { Image(systemName: "bookmark.fill")}.tag(TabModel.Tab.pastTravel)
+                MainTravelView().tabItem { Image(systemName: "airplane") }.tag(TabModel.Tab.mainTravel)
+                Text("tabcontent 3 ").tabItem { Image(systemName: "person.fill")}.tag(TabModel.Tab.myPage)
+            }
+            .toolbarBackground(.black, for: .tabBar)
+            .toolbarBackground(.visible, for: .tabBar)
+            
         }
-        .padding()
     }
 }
 
+@Observable
+class TabModel {
+    var selectedTab: Tab
+    enum Tab {
+        case pastTravel
+        case mainTravel
+        case myPage
+    }
+    init(selectedTab: Tab = .mainTravel) {
+        self.selectedTab = .mainTravel
+    }
+}
 #Preview {
     ContentView()
 }
---
+++ Boleto/Boleto/MainTravelView.swift
@@ -0,0 +1,89 @@
+//
+//  MainTravelView.swift
+//  Boleto
+//
+//  Created by Sunho on 8/11/24.
+//
+
+import SwiftUI
+
+struct MainTravelView: View {
+    @State var currentTab: Int = 0
+    @Namespace var namespace
+    var tabbarOptions: [String] = ["티켓", "추억"]
+    var body: some View {
+        VStack {
+            HStack {
+                HStack(alignment: .top){
+                    ForEach(tabbarOptions.indices, id: \.self) {index in
+                        let title = tabbarOptions[index]
+                        TravelTabbaritem(currentTab: $currentTab, namespace: namespace, title: title, tab: index)
+                    }
+                }
+                .frame(width: 80, height: 40)
+                Spacer()
+            }.padding()
+            VStack {
+                if currentTab == 0{
+                    TicketsView()
+                }else {
+                    MemoriesView()
+                }
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
+                .animation(.easeInOut, value: currentTab)
+        }
+    }
+}
+
+
+#Preview {
+    MainTravelView()
+}
+struct TicketsView: View {
+    var body: some View {
+        // 티켓 탭에 대한 콘텐츠
+        Text("티켓 내용 표시")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.blue.opacity(0.2))
+            .cornerRadius(10)
+    }
+}
+
+struct MemoriesView: View {
+    var body: some View {
+        // 추억 탭에 대한 콘텐츠
+        Text("추억 내용 표시")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.green.opacity(0.2))
+            .cornerRadius(10)
+    }
+}
+
+struct TravelTabbaritem: View {
+    @Binding var currentTab: Int
+    let namespace: Namespace.ID
+    var title: String
+    var tab: Int
+    
+    var body: some View {
+        Button {
+            currentTab = tab
+        } label: {
+            VStack {
+                Spacer()
+                if currentTab == tab {
+                    Text(title)
+                        .foregroundStyle(Color.orange)
+                    Color.orange.frame(height: 2)
+                        .matchedGeometryEffect(id: "underline", in: namespace.self)
+                } else {
+                    Text(title)
+                        .foregroundStyle(Color.gray)
+                    Color.clear.frame(height: 2)
+                }
+            }
+            .animation(.spring(), value: currentTab)
+        }.buttonStyle(.plain)
+    }
+}
