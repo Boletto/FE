fced8622db604ced7e8395a55a80c610ff647133
Sunday <sunhofficial@github.com>
2024-09-16 21:26:55 +0900

    feat: ticket Keyword

--- Boleto/Boleto/Components/TicketView.swift
+++ Boleto/Boleto/Components/TicketView.swift
@@ -68,8 +68,19 @@ struct TicketView: View {
                         .frame(width: 1,height: 56)
                         .padding(.trailing,14)
                         .padding(.vertical, 2)
-                    Text(ticket.startDate)
-                        .font(.customFont(ticket.keywords[0].boldfont, size: 21))
+               
+                
+                    FlowLayout(hspacing: 7, vSpacing: 6) {
+                            ForEach(ticket.keywords, id: \.self) { keyword in
+                                Text(keyword.rawValue)
+                                    .lineLimit(1)
+                                    .font(.customFont(ticket.keywords[0].regularfont, size: 10))
+                                    .padding(.vertical, 4)
+                                    .padding(.horizontal, 11)
+                                    .background(Capsule().stroke(style: StrokeStyle(lineWidth: 1)))
+                            }
+                        }
+                 
                 }
                 Rectangle().frame(height: 1)
                     .padding(.bottom, 21)
@@ -151,6 +162,67 @@ struct TicketView: View {
 }
 
 #Preview {
-    TicketView(ticket: Ticket(departaure: "Seoul", arrival: "Busan", startDate: "2024.1.28", endDate: "2024.04.12", participant: [Person(image: "beef3", name: "강병호"),Person(image: "beef1", name: "김수민"),Person(image: "beef2", name: "하잇"),Person(image: "beef4", name: "면답"), Person(image: "beef2", name: "호잇")], keywords: [.activity]))
+    TicketView(ticket: Ticket(departaure: "Seoul", arrival: "Busan", startDate: "2024.1.28", endDate: "2024.04.12", participant: [Person(image: "beef3", name: "강병호"),Person(image: "beef1", name: "김수민"),Person(image: "beef2", name: "하잇"),Person(image: "beef4", name: "면답"), Person(image: "beef2", name: "호잇")], keywords: [.activity,.shopping,.backpacking]))
     
+}
+struct FlowLayout: Layout {
+    var hspacing: CGFloat
+    var vSpacing: CGFloat
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        let subSizes = subviews.map { $0.sizeThatFits(proposal) }
+
+        let proposedWidth = proposal.width ?? .infinity
+        var maxRowWidth = CGFloat.zero
+        var rowCount = CGFloat.zero
+        var x = CGFloat.zero
+        for subSize in subSizes {
+            // This prevents empty rows if any subviews are wider than proposedWidth.
+            let lineBreakAllowed = x > 0
+
+            if lineBreakAllowed, x + subSize.width + hspacing > proposedWidth {
+                rowCount += 1
+                x = 0
+            }
+
+            x += subSize.width + hspacing
+            maxRowWidth = max(maxRowWidth, x - hspacing)
+        }
+
+        if x > 0 {
+            rowCount += 1
+        }
+
+        let rowHeight = subSizes.lazy.map { $0.height }.max() ?? 0
+        return CGSize(
+            width: proposal.width ?? maxRowWidth,
+            height: rowCount * rowHeight
+        )
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        let subSizes = subviews.map { $0.sizeThatFits(proposal) }
+        let rowHeight = subSizes.lazy.map { $0.height }.max() ?? 0
+        let proposedWidth = proposal.width ?? .infinity
+
+        var p = CGPoint.zero
+        for (subview, subSize) in zip(subviews, subSizes) {
+            // This prevents empty rows if any subviews are wider than proposedWidth.
+            let lineBreakAllowed = p.x > 0
+
+            if lineBreakAllowed, p.x + subSize.width + hspacing > proposedWidth {
+                p.x = 0
+                p.y += rowHeight + vSpacing
+            }
+
+            subview.place(
+                at: CGPoint(
+                    x: bounds.origin.x + p.x,
+                    y: bounds.origin.y + p.y + 0.5 * (rowHeight - subSize.height)
+                ),
+                proposal: proposal
+            )
+
+            p.x += subSize.width + hspacing
+        }
+    }
 }
