5bcec9d243416abb00915e3bb36f08d482085257
Sunday <sunhofficial@github.com>
2024-09-18 18:17:01 +0900

    feat: Fourcut Frame NotiView with feature

---
+++ Boleto/Boleto/BadgeNotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeNotificationView.swift
+//  Boleto
+//
+//  Created by Sunho on 9/18/24.
+//
+
+import SwiftUI
+
+struct BadgeNotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeNotificationView()
+}
