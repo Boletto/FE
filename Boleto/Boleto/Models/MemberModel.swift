f5d6061416db8513e96559d70c8db1402e0c1f58
Sunday <sunhofficial@github.com>
2024-11-19 09:02:23 +0900

    feat: add friendSelectionView but going to refactoring

--- Boleto/Boleto/Models/MemberModel.swift
+++ Boleto/Boleto/Models/MemberModel.swift
@@ -13,5 +13,10 @@ struct MemberModel: Equatable, Identifiable {
     let imageUrl: String?
 }
 extension MemberModel{
-    static let dummy = MemberModel(id: 12, name: "선호", nickname: "선데이", imageUrl: "")
+    static let dummy = MemberModel(id: 12, name: "선호", nickname: "선데이", imageUrl: nil)
+    static let dummyList: [MemberModel] = [.init(id: 13, name: "현우", nickname: "woody", imageUrl: nil),
+                                           .init(id: 15, name: "은서", nickname: "응짝", imageUrl: nil),
+                                           .init(id: 17, name: "지우", nickname: "주주주주베베베", imageUrl: nil),
+                                           .init(id: 18, name: "선호", nickname: "개쩐다", imageUrl: nil)]
+    
 }
--- Boleto/Boleto/Views/AddTravel/AddBottomSheet/FriendSelection/FriendSelectionFeature.swift
+++ Boleto/Boleto/Views/AddTravel/AddBottomSheet/FriendSelection/FriendSelectionFeature.swift
@@ -14,13 +14,14 @@ struct FriendSelectionFeature {
     struct State: Equatable {
         var friends = [MemberModel]()
         var searchText: String = ""
-        var selectedFriends : [MemberModel]
+        var selectedFriends : [MemberModel] = []
         var filteredFriends: [MemberModel] {
-                  if searchText.isEmpty {
-                      return friends
-                  }
-                  return friends.filter { $0.nickname.contains(searchText) }
-              }
+            if searchText.isEmpty {
+                return friends
+            }
+            return friends.filter { $0.nickname.contains(searchText) || $0.name.contains(searchText)}
+        }
+        var shareCode:  String = ""
         
     }
     enum Action: BindableAction, Equatable {
@@ -34,23 +35,17 @@ struct FriendSelectionFeature {
         case sendFriendId
     }
     @Dependency(\.dismiss) var dismiss
-    @Dependency(\.userClient) var userClient
+    @Dependency(\.friendClient) var friendClient
     var body: some ReducerOf<Self> {
         BindingReducer()
         Reduce { state, action in
             switch action {
-            case .binding(\.searchText):
-                return .run { send in
-                    await send(.searchFriends)
-                        
-                }.debounce(id: "SearchDebounce", for: .milliseconds(300), scheduler: DispatchQueue.main)
-//                return .none
-
+                
             case .binding:
                 return .none
             case .searchFriends:
-//                let filteredFriends = state.friends.filter {$0.contains(state.searchText)}
-//                state.resultFriends = filteredFriends
+                //                let filteredFriends = state.friends.filter {$0.contains(state.searchText)}
+                //                state.resultFriends = filteredFriends
                 return .none
             case .tapxmark:
                 return .run { _ in
@@ -61,21 +56,20 @@ struct FriendSelectionFeature {
                 state.searchText = ""
                 return .none
             case .fetchFriend :
-//                return .run {send in
-//                    let friends = try await userClient.getFriends()
-//                    await send(.updateResultFriends(friends))
-//                }
-                return .none
+                return .run {send in
+                    let friends = try await friendClient.getAllFriends()
+                    await send(.updateResultFriends(friends))
+                }
             case .updateResultFriends(let friends):
                 state.friends = friends
                 return .none
             case .toggleFriendSelection(let friend):
                 if let index = state.selectedFriends.firstIndex(where: { $0.id == friend.id }) {
-                                   state.selectedFriends.remove(at: index)
-                               } else {
-                                   state.selectedFriends.append(friend)
-                               }
-                    return .none
+                    state.selectedFriends.remove(at: index)
+                } else {
+                    state.selectedFriends.append(friend)
+                }
+                return .none
             case .sendFriendId:
                 return .none
             }
