b59ba4d8e7e20a81b63771b81a01ce39ca59e4d8
Sunday <sunhofficial@github.com>
2024-12-19 20:20:10 +0900

    refactor: modify spot

--- Boleto/Boleto/Models/Spots/SuwonSpot.swift
+++ Boleto/Boleto/Models/Spots/SuwonSpot.swift
@@ -13,17 +13,16 @@ struct SuwonSpot: Spot {
     var upperString: String { "SUWON" }
     var coordinate: CLLocationCoordinate2D {
 
-        CLLocationCoordinate2D(latitude: 37.2432, longitude: 127.0798)
+        CLLocationCoordinate2D(latitude: 37.26357, longitude: 127.0286)
     }
     var landmarks: [Badge] {
         return [
            
-            Badge(badgetype: .sw04, latitude: 37.24127, longtitude: 127.0809),  // 월드컵경기장
-            Badge(badgetype: .sw01, latitude: 37.2652416, longtitude: 127.0377224), // 월화원
+            Badge(badgetype: .sw01, latitude: 37.265271, longtitude: 127.0377224), // 월화원
 
             Badge(badgetype: .sw02, latitude: 37.2871202, longtitude: 127.0119379), // 수원화성
             Badge(badgetype: .sw03, latitude: 37.2830911, longtitude: 127.0659215), // 광교호수공원
-//            Badge(badgetype: .sw04, latitude: 37.2864742, longtitude: 127.036866)  // 월드컵경기장
+            Badge(badgetype: .sw04, latitude: 37.2864742, longtitude: 127.036866)  // 월드컵경기장
         ]
     }
 }
